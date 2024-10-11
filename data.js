91cb9944321bfb1c4453d4263e05afc5970389c5
Nikita P <petrowitch.nickita2018@yandex.ru>
2024-10-11 03:23:37 +0300

    11.10.2024 done

--- data.js
+++ data.js
@@ -1,51 +1,110 @@
-const images = [{
+const cats = [{
   'id': 'YdAqiUkUoWA',
   'url': 'img/cats-1.jpg',
-  'description': 'pink petaled flower',
+  'description': '',
 }, {
   'id': 'hX_hf2lPpUU',
   'url': 'img/cats-2.jpg',
-  'description': 'green leaf',
+  'description': '',
 }, {
   'id': 'w1JE5duY62M',
   'url': 'img/cats-3.jpg',
-  'description': 'red and white petaled flower close-up photography',
+  'description': '',
 }, {
   'id': '3tYZjGSBwbk',
   'url': 'img/cats-4.jpg',
-  'description': 'white daisy in bloom during daytime',
+  'description': '',
 }, {
   'id': 'NoXUQ54pDac',
   'url': 'img/cats-5.jpg',
-  'description': 'white-and-pink flowers',
+  'description': '',
 }, {
   'id': 'OZhYgZh0bAg',
   'url': 'img/cats-6.jpg',
-  'description': 'white and purple flower petals',
-}, {
-  'id': 'YdAqiUkUoWA1',
-  'url': 'img/cats-1.jpg',
-  'description': 'pink petaled flower',
-}, {
-  'id': 'hX_hf2lPpUU1',
-  'url': 'img/cats-2.jpg',
-  'description': 'green leaf',
-}, {
-  'id': 'w1JE5duY62M1',
-  'url': 'img/cats-3.jpg',
-  'description': 'red and white petaled flower close-up photography',
-}, {
-  'id': '3tYZjGSBwbk1',
-  'url': 'img/cats-4.jpg',
-  'description': 'white daisy in bloom during daytime',
-}, {
-  'id': 'NoXUQ54pDac1',
-  'url': 'img/cats-5.jpg',
-  'description': 'white-and-pink flowers',
-}, {
-  'id': 'OZhYgZh0bAg1',
-  'url': 'img/cats-6.jpg',
-  'description': 'white and purple flower petals',
+  'description': '',
 }];
 
-window.words = words;
+const cars = [{
+  'id': 'YdAqiUkUoWA',
+  'url': 'img/cars-1.jpg',
+  'description': '',
+}, {
+  'id': 'hX_hf2lPpUU',
+  'url': 'img/cars-2.jpg',
+  'description': '',
+}, {
+  'id': 'w1JE5duY62M',
+  'url': 'img/cars-3.jpg',
+  'description': '',
+}, {
+  'id': '3tYZjGSBwbk',
+  'url': 'img/cars-4.jpg',
+  'description': '',
+}, {
+  'id': 'NoXUQ54pDac',
+  'url': 'img/cars-5.jpg',
+  'description': '',
+}, {
+  'id': 'OZhYgZh0bAg',
+  'url': 'img/cars-6.jpg',
+  'description': '',
+}];
+
+const flowers = [{
+  'id': 'YdAqiUkUoWA',
+  'url': 'img/flowers-1.jpg',
+  'description': '',
+}, {
+  'id': 'hX_hf2lPpUU',
+  'url': 'img/flowers-2.jpg',
+  'description': '',
+}, {
+  'id': 'w1JE5duY62M',
+  'url': 'img/flowers-3.jpg',
+  'description': '',
+}, {
+  'id': '3tYZjGSBwbk',
+  'url': 'img/flowers-4.jpg',
+  'description': '',
+}, {
+  'id': 'NoXUQ54pDac',
+  'url': 'img/flowers-5.jpg',
+  'description': '',
+}, {
+  'id': 'OZhYgZh0bAg',
+  'url': 'img/flowers-6.jpg',
+  'description': '',
+}];
+
+const imageCollection = {
+  flowers, cats, cars,
+};
+
+const results = [
+  { name: 'Аня', stepsCount: 16 },
+  { name: 'Вася', stepsCount: 12 },
+  { name: 'Петя', stepsCount: 19 }
+];
+
+
+
+const getImages = (type) => {
+  let secondArr = [...imageCollection[type]]
+  secondArr = secondArr.map(item =>{
+    return {...item, id: item.id+'-1'}
+  })
+  let newArr = [...imageCollection[type], ...secondArr];
+  for (i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr
+} 
+
+// RANDOMIZED ? [...imageCollection[type]].sort(() => 0.5 - Math.random()) : [...imageCollection[type]]
+
+//   {
+//   // TODO: напишите код для формирования тематического набора данных
+//   // TODO: напишите код для перемешивания данных
+//   return [];
+// };
--- settings.js
+++ settings.js
@@ -1,4 +1,20 @@
 // Файл для настроек приложения
 const TIMEOUT = 800;
 
+
+
+const AppRoute = {
+    Initial: 'initial',
+    Game: 'game',
+    Results: 'results',
+  };
+
+const GAME_TYPES = [
+  {type: 'cats', icon: 'cats', label: 'Котики'},
+  {type: 'flowers', icon: 'flowers', label: 'Цветочки'},
+  {type: 'cars', icon: 'cars', label: 'Машины'},
+]
+
 window.TIMEOUT = TIMEOUT;
+window.GAME_TYPES = GAME_TYPES;
+const RANDOMIZED = false;
